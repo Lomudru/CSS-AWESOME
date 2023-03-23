76b604d6815a3375274cd23064c065a3ca4562cb
Lomudru <kmejasson@gmail.com>
2023-03-23 11:12:04 +0100

    Avancement dans le php

--- src/models/iconManager.php
+++ src/models/iconManager.php
@@ -13,4 +13,17 @@ class iconManager
         $this->connexion = new \PDO('mysql:host=' . HOST . ';dbname=' . DATABASE . ';charset=utf8;', USER, PASSWORD);
         $this->connexion->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
     }
+
+    public function getAll(){
+        $stmt = $this->prepare('SELECT * FROM icon');
+        $stmt->execute(array());
+        return $stmt->fetchAll(\PDO::FETCH_CLASS, "Cssawesome\models\icon");
+    }
+
+    public function getIcon($id){
+        $stmt = $this->connexion->prepare('SELECT * FROM icon WHERE icon_id = :idicon');
+        $stmt->bindParam(':idicon', $id, \PDO::PARAM_STR);
+        $stmt->execute();
+        return $stmt->fetchAll(\PDO::FETCH_CLASS, "Cssawesome\models\icon");
+    }
 }
